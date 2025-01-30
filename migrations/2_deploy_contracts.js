f49dfbfd7cb3edfb5a8c311d38b7edf799f29d5c
Aziz Ahmed <aziz.ahmed@urbanstems.com>
2025-01-30 16:59:00 -0600

    testing capturing  error during deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage, "sim"); // We set the initial value to 100
+  deployer.deploy(SimpleStorage, 100); // We set the initial value to 100
 };
