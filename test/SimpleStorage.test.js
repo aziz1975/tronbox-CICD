b669e921b9f92dfcdd0f7753a6c1314a0109279d
Aziz Ahmed <aziz.ahmed@urbanstems.com>
2025-01-31 10:31:52 -0600

    testin happy path

--- test/SimpleStorage.test.js
+++ test/SimpleStorage.test.js
@@ -7,7 +7,7 @@ contract("SimpleStorage", accounts => {
     assert.equal(value.toNumber(), 100, "Initial value should be 100");
   });
 
-  it("should update the stored value", async () => {
+  it("should update the stored value.", async () => {
     const instance = await SimpleStorage.deployed();
     await instance.set(123);
     const newValue = await instance.get();
