15583b8751c9a72ec7a4ced9795832b322f9894a
Aziz Ahmed <aziz.ahmed@urbanstems.com>
2025-02-27 21:10:13 -0600

    Contract verification testing

--- test/SimpleStorage.test.js
+++ test/SimpleStorage.test.js
@@ -4,13 +4,14 @@ contract("SimpleStorage", accounts => {
   it("should store the correct initial value", async () => {
     const instance = await SimpleStorage.deployed();
     const value = await instance.get();
-    assert.equal(value.toNumber(), 100, "Initial value should be 100");
+    assert.equal(value, 100, "Initial value should be 100");
   });
 
   it("should update the stored value.", async () => {
     const instance = await SimpleStorage.deployed();
     await instance.set(123);
     const newValue = await instance.get();
-    assert.equal(newValue.toNumber(), 123, "Value should be updated to 123");
+    assert.equal(newValue, 123, "Value should be updated to 123");
+
   });
 });
